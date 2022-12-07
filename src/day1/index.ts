63ac012399a041bdc8f4447827e2c41bc8a67120
Ryszard Matula <ryszard.matula.dev@gmail.com>
2022-12-07 13:07:07 +0100

    Day 1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -2,8 +2,20 @@ import { readFile } from 'fs/promises'
 import { join } from 'path'
 
 const main = async () => {
-  const result = await readFile(join(__dirname, 'input-small.txt'), 'utf-8')
-  console.log(result + 'xdff')
+  const fileResult = await readFile(join(__dirname, 'input.txt'), 'utf-8')
+  let max = 0
+  let calories = 0
+
+  fileResult.split('\n').forEach((line) => {
+    calories = Number(line) + calories
+
+    if (line === '') {
+      max = calories > max ? calories : max
+      calories = 0
+    }
+  })
+
+  console.log(`Result: ${max}`)
 }
 
 main()
