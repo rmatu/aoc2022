30845a0af936361b22482658a7d3afb3f58007a3
Ryszard Matula <ryszard.matula.dev@gmail.com>
2022-12-10 11:59:54 +0100

    Day 6 part 2

--- src/day6/partTwo.ts
+++ src/day6/partTwo.ts
@@ -3,8 +3,26 @@ import { join } from 'path'
 
 export const main = async () => {
   const fileResult = await readFile(join(__dirname, 'input.txt'), 'utf-8')
-  const lines = fileResult.split('\n')
-  const result = 0
+  const allCharacters = fileResult.split('')
 
-  console.log(`Result: ${result}`)
+  const packet: string[] = []
+  let markerPosition = 0
+
+  allCharacters.every((char, idx) => {
+    packet.push(char)
+
+    if (idx < 14) return true
+
+    packet.shift()
+    const set = new Set(packet)
+
+    if (set.size === 14) {
+      markerPosition = idx + 1
+      return false
+    }
+
+    return true
+  })
+
+  console.log(`Result: ${markerPosition}`)
 }
